5d1e243a8d44102e8510a8f66cdce6f8380e8f15
vamsi9710 <78356010+vamsi9710@users.noreply.github.com>
2021-02-03 13:11:48 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,47 @@
+//function for pushing elements into stack
+void push(int valu)
+{
+    if(top==MAX-1)
+    {
+        printf("\nstack is full\n");
+    }
+    else
+    {
+        top=top+1;
+        stack[top]=valu;
+    }
+}
 
+
+
+//function for poping elements from stack
+void pop()
+{
+    if(top==-1)
+    {
+        printf("\n stack is empty\n");
+    }
+    else
+    {
+        printf("\ndeleted element is %d",stack[top]);
+        top=top-1;
+    }
+}
+
+
+
+//function for displaying elements in the stack
+void display()
+{
+    int i;
+    if(top==-1)
+    {
+        printf("\nstack is empty\n");
+    }
+    else
+    {
+        printf("\nelements in the stack:");
+        for(i=top;i>=0;--i)
+            printf("%d\n",stack[i]);
+    }
+}
